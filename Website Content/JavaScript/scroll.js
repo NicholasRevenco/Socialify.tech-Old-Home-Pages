ff26e344c1cc71e45033184ce8ec5fce03f83a41
NicholasRevenco <nicholas.revenco.25@bishops.com>
2022-11-21 23:12:04 -0800

    Development into the navbar color change proof of concept

--- Website Content/JavaScript/scroll.js
+++ Website Content/JavaScript/scroll.js
@@ -1,13 +1,15 @@
-console.log('hello!');
 
 const header = document.querySelector('.header');
+const header2 = document.querySelector('.header2');
 
 window.onscroll = function() {
   var top = window.scrollY;
   console.log(top);
   if (top >= 100) {
     header.classList.add('active')
+    header2.classList.add('active')
   } else {
     header.classList.remove('active');
+    header2.classList.remove('active');
   }
 }
