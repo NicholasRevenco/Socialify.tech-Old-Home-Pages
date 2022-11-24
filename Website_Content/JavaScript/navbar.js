405d3c706ac62d6f4f95a8c7da3c770e69521401
NicholasRevenco <nicholas.revenco.25@bishops.com>
2022-11-24 09:41:31 -0800

    On small screen navbar will be black

--- Website_Content/JavaScript/navbar.js
+++ Website_Content/JavaScript/navbar.js
@@ -2,9 +2,11 @@
 
 const header = document.querySelector('.header');
 
+/* Do not know how to resolve this error */
 window.onscroll = function() {
   var top = window.scrollY;
-  if (top >= 50) {
+  if (top >= 50 || past > 0) {
+    const past = 1
     header.classList.add('active')
   } else {
     header.classList.remove('active');
@@ -13,11 +15,20 @@ window.onscroll = function() {
 
 window.onresize = function() {
   var width = window.innerWidth;
-  if (width < 576) {
+  if (width < 577) {
     header.classList.add('active')
   } else {
     header.classList.remove('active');
   }
 };
+
+window.onload = function() {
+  var width = window.innerWidth;
+  if (width <= 577) {
+    header.classList.add('active')
+  } else {
+    header.classList.remove('active');
+  }
+}
 
 /* End here */
