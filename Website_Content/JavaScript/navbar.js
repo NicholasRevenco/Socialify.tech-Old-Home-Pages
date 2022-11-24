ffa7e16e36c43a925152a38a28b97be5b564ac12
NicholasRevenco <nicholas.revenco.25@bishops.com>
2022-11-24 12:11:47 -0800

    Change color of navbar links when scroll

--- Website_Content/JavaScript/navbar.js
+++ Website_Content/JavaScript/navbar.js
@@ -1,14 +1,40 @@
 /* This is required to make the navbar change color when scrolled */
 
-const navbar_change = document.querySelector('.navbar_change');
+/* Need to find a way to call all classes */
 
-/* Do not know how to resolve this error */
+/*const navbar_change = document.querySelector('.navbar_change');
+const navbar_link_scroll = document.querySelectorAll('.navbar_link_scroll');
+
+
 window.onscroll = function() {
   var top = window.scrollY;
   if (top >= 50) {
     navbar_change.classList.add('active')
+    navbar_link_scroll.classList.add('active');
   } else {
     navbar_change.classList.remove('active');
+    navbar_link_scroll.classList.remove('active');
+  }
+}*/
+
+const navbar_change = document.querySelector('.navbar_change');
+const navbar_link_scroll_1 = document.querySelector('.navbar_link_scroll_1');
+const navbar_link_scroll_2 = document.querySelector('.navbar_link_scroll_2');
+const navbar_link_scroll_3 = document.querySelector('.navbar_link_scroll_3');
+
+
+window.onscroll = function() {
+  var top = window.scrollY;
+  if (top >= 50) {
+    navbar_change.classList.add('active')
+    navbar_link_scroll_1.classList.add('active');
+    navbar_link_scroll_2.classList.add('active');
+    navbar_link_scroll_3.classList.add('active');
+  } else {
+    navbar_change.classList.remove('active');
+    navbar_link_scroll_1.classList.remove('active');
+    navbar_link_scroll_2.classList.remove('active');
+    navbar_link_scroll_3.classList.remove('active');
   }
 }
 
