75ce0dc7a12948c0238a53ee5f2732703624def1
NicholasRevenco <nicholas.revenco.25@bishops.com>
2022-11-24 12:29:36 -0800

    Change of color of navbar collapse button when scroll

--- Website_Content/JavaScript/navbar.js
+++ Website_Content/JavaScript/navbar.js
@@ -22,7 +22,9 @@ const navbar_link_scroll_1 = document.querySelector('.navbar_link_scroll_1');
 const navbar_link_scroll_2 = document.querySelector('.navbar_link_scroll_2');
 const navbar_link_scroll_3 = document.querySelector('.navbar_link_scroll_3');
 const navbar_button = document.querySelector('.navbar_button');
-
+const bar_change_1 = document.querySelector('.bar_change_1');
+const bar_change_2 = document.querySelector('.bar_change_2');
+const bar_change_3 = document.querySelector('.bar_change_3');
 
 window.onscroll = function() {
   var top = window.scrollY;
@@ -32,12 +34,18 @@ window.onscroll = function() {
     navbar_link_scroll_2.classList.add('active');
     navbar_link_scroll_3.classList.add('active');
     navbar_button.classList.add('active');
+    bar_change_1.classList.add('active');
+    bar_change_2.classList.add('active');
+    bar_change_3.classList.add('active');
   } else {
     navbar_change.classList.remove('active');
     navbar_link_scroll_1.classList.remove('active');
     navbar_link_scroll_2.classList.remove('active');
     navbar_link_scroll_3.classList.remove('active');
     navbar_button.classList.remove('active');
+    bar_change_1.classList.remove('active');
+    bar_change_2.classList.remove('active');
+    bar_change_3.classList.remove('active');
   }
 }
 
@@ -45,18 +53,24 @@ window.onresize = function() {
   var width = window.innerWidth;
   if (width < 577) {
     navbar_change.classList.add('active')
+    bar_change_1.classList.add('active');
+    bar_change_2.classList.add('active');
+    bar_change_3.classList.add('active');
   } else {
     navbar_change.classList.remove('active');
+    bar_change_1.classList.remove('active');
+    bar_change_2.classList.remove('active');
+    bar_change_3.classList.remove('active');
   }
-};
+}
 
-window.onload = function() {
+/*window.onload = function() {
   var width = window.innerWidth;
-  if (width <= 577) {
+  if (width <= 600) {
     navbar_change.classList.add('active')
   } else {
     navbar_change.classList.remove('active');
   }
-}
+}*/
 
 /* End here */
